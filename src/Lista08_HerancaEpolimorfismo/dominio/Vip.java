e1d0081b5ac79b0d95a6772eb67d93d53e150834
Asafe Orneles <asafeorneles@gmail.com>
2025-04-03 01:26:46 -0300

    lista8 commit

--- src/Lista08_HerancaEpolimorfismo/dominio/Vip.java
+++ src/Lista08_HerancaEpolimorfismo/dominio/Vip.java
@@ -2,7 +2,6 @@ package Lista08_HerancaEpolimorfismo.dominio;
 
 public abstract class Vip extends Ingresso{
     public static final double VALOR_ADICIONAL_VIP = 35;
-    protected double valorVip;
 
     @Override
     public void imprime() {
