eb2f52b27be4ead84e458882660429e5e32cf499
Asafe Orneles <asafeorneles@gmail.com>
2025-04-03 01:25:14 -0300

    lista8 commit

--- src/Lista08_HerancaEpolimorfismo/test/IngressoTest02.java
+++ src/Lista08_HerancaEpolimorfismo/test/IngressoTest02.java
@@ -1,6 +1,6 @@
 package Lista08_HerancaEpolimorfismo.test;
-
 import Lista08_HerancaEpolimorfismo.dominio.*;
+
 
 import java.util.Scanner;
 
@@ -8,18 +8,37 @@ public class IngressoTest02 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        int opcao = escolherIngresso(entrada);
-
-    }
-
-    public static void escolherIngresso(Scanner entrada) {
+        int opcao;
         do {
             System.out.println("Escolha seu ingresso (Digite 1 para Normal e 2 para Vip): ");
-            int opcao = entrada.nextInt();
+            opcao = entrada.nextInt();
+
             if (opcao == 1) {
                 Normal normal = new Normal();
                 normal.imprime();
             }
-        }
+            else if (opcao == 2) {
+                int opcao2;
+                do {
+                    System.out.println("Ingresso VIP selecionado!");
+                    System.out.println("Escolha a localização (Digite 1 para Inferior e 2 para Superior): ");
+                    opcao2 = entrada.nextInt();
+
+                    if (opcao2 == 1) {
+                        CamaroteInferior camaroteInferior = new CamaroteInferior();
+                        camaroteInferior.imprime();
+                    }
+                    else if (opcao2 == 2) {
+                        CamaroteSuperior camaroteSuperior = new CamaroteSuperior();
+                        camaroteSuperior.imprime();
+                    } else {
+                        System.out.println("Opção inválida! Tente novamente.");
+                    }
+                } while (opcao2 != 1 && opcao2 != 2);
+            } else {
+                System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (opcao != 1 && opcao != 2);
+
     }
 }
